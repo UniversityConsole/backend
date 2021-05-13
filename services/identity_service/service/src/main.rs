ebb0fd6dede2af044b4f2731cc6f66198bd4eada
Victor Barbu <vicbarbu@pm.me>
2021-05-13 22:23:04 +0300

    Add logging utility

--- services/identity_service/service/src/main.rs
+++ services/identity_service/service/src/main.rs
@@ -1,13 +1,26 @@
-use std::collections::HashMap;
+extern crate simple_logger;
+extern crate log;
+
+use std::{collections::HashMap, env};
 
 use lambda_http::{IntoResponse, Request, handler, http::Method};
 use lambda_http::lambda_runtime::{self, Context};
 use serde::{Serialize, Deserialize};
+use simple_logger::SimpleLogger;
+use log::LevelFilter;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let debug_enabled = env::var("LAMBDA_DEBUG").is_ok();
+    let log_level = if debug_enabled { LevelFilter::Debug } else { LevelFilter::Info };
+
+    SimpleLogger::new()
+        .with_module_level(module_path!(), log_level)
+        .init()
+        .unwrap();
+
     lambda_runtime::run(handler(process_request)).await?;
     Ok(())
 }
@@ -54,6 +67,10 @@ async fn process_request(request: Request, _: Context) -> Result<impl IntoRespon
 
     let method = request.method();
     let uri = &request.uri().path()[URI_SCOPE.len()..];
+
+    log::debug!("Got raw URI: {}", &request.uri());
+    log::debug!("Processed URI: {}", &uri);
+    log::debug!("Got method: {}", &method);
 
     match (method, uri) {
         (&Method::GET, "/accounts") => list_accounts(&request).await,
