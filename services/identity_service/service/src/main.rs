450aad4a81e98e10009b79aa92a98245f7969779
Victor Barbu <victorbarbu08@gmail.com>
2021-05-23 14:52:52 +0300

    [IdentityService] Add CORS Access-Control-Allow-Origin to response

--- services/identity_service/service/src/main.rs
+++ services/identity_service/service/src/main.rs
@@ -5,7 +5,7 @@ extern crate simple_logger;
 
 use std::env;
 
-use lambda_http::lambda_runtime::{self, Context as LambdaRuntimeContext};
+use lambda_http::{http::header::ACCESS_CONTROL_ALLOW_ORIGIN, lambda_runtime::{self, Context as LambdaRuntimeContext}};
 use lambda_http::{handler, http::Method, IntoResponse, Request};
 use log::LevelFilter;
 use rusoto_core::Region;
@@ -48,7 +48,12 @@ async fn main() -> Result<(), Error> {
         .init()
         .unwrap();
 
-    lambda_runtime::run(handler(process_request)).await?;
+    lambda_runtime::run(handler(|req, ctx| async {
+        let mut res = process_request(req, ctx).await?.into_response();
+        let headers = res.headers_mut();
+        headers.insert(ACCESS_CONTROL_ALLOW_ORIGIN, "*".parse().unwrap());
+        Ok(res)
+    })).await?;
     Ok(())
 }
 
