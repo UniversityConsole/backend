44f5bc068b5a8d61a95ce5d1036b690910a4ac97
Victor Barbu <vicbarbu@pm.me>
2022-05-02 16:35:10 +0100

    [core] Rename all JWT Claims fields to camelCase on serialization

--- services/core/src/auth/jwt.rs
+++ services/core/src/auth/jwt.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Claims {
     pub sub: String,
     pub email: String,
