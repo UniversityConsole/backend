9f4ae8afad6c12197ef28b324162c0c6b7ce73f7
Victor Barbu <vicbarbu@pm.me>
2022-07-02 20:10:30 +0100

    [identity_service] Replace SyncLazy with LazyLock
    Feature "once_cell" is described in rust-lang/rust#74465.
    
    As part of flip1995/rust-clippy@4737e9e42bdece36eed33fd9639195b60fdacd6f the
    names of SyncLazy et al. were changed. In particular, SyncLazy was changed to
    LazyLock.

--- services/identity_service/src/permissions/anonymous.rs
+++ services/identity_service/src/permissions/anonymous.rs
@@ -1,11 +1,11 @@
-use std::lazy::SyncLazy;
+use std::sync::LazyLock;
 
 use service_core::resource_access::{AccessKind, PolicyStatement};
 
 use crate::permissions::helper::compose_statement;
 
 /// Permissions given to anonymous entities.
-pub static ANONYMOUS_PERMISSIONS: SyncLazy<Vec<PolicyStatement>> = SyncLazy::new(|| {
+pub static ANONYMOUS_PERMISSIONS: LazyLock<Vec<PolicyStatement>> = LazyLock::new(|| {
     const ALLOWED_MUTATIONS: [&str; 1] = ["authenticate(email: *, password: *)::*"];
 
     vec![compose_statement(AccessKind::Mutation, ALLOWED_MUTATIONS)]
--- services/identity_service/src/permissions/default.rs
+++ services/identity_service/src/permissions/default.rs
@@ -1,4 +1,4 @@
-use std::lazy::SyncLazy;
+use std::sync::LazyLock;
 
 use service_core::resource_access::{AccessKind, PolicyStatement};
 
@@ -6,7 +6,7 @@ use crate::permissions::helper::compose_statement;
 
 
 /// Permissions given to authenticated entities by default.
-pub static DEFAULT_PERMISSIONS: SyncLazy<Vec<PolicyStatement>> = SyncLazy::new(|| {
+pub static DEFAULT_PERMISSIONS: LazyLock<Vec<PolicyStatement>> = LazyLock::new(|| {
     const ALLOWED_MUTATIONS: [&str; 1] = ["generateAccessToken(refreshToken: *)::*"];
 
     vec![compose_statement(AccessKind::Mutation, ALLOWED_MUTATIONS)]
